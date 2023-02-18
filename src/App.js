275a13a77e9ad874b3d9f794c1b108db86369fd8
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-02-18 10:53:51 +0300

    Updated code

--- src/App.js
+++ src/App.js
@@ -6,11 +6,10 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState()
 
-  function fetchMoviesHandler () {
-    fetch('https://swapi.dev/api/films/').then(response => {
-      return response.json()
-    })
-    .then(data => {
+  async function fetchMoviesHandler () {
+    const response = await fetch('https://swapi.dev/api/films/')
+      const data = await response.json()
+
       const transformedMovies = data.results.map(movieData => {
         return {
           id: movieData.episode_id,
@@ -21,9 +20,8 @@ function App() {
       })
 
       setMovies(transformedMovies)
-    })
   }
- 
+
   return (
     <React.Fragment>
       <section>
